033b3d82a4166d4dacafc7847365b4a8a9441c37
Subin Kim <70880961+Ssubini@users.noreply.github.com>
2023-10-06 12:18:01 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 132 ms, Memory: 115004 KB -BaekjoonHub

---
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+answer = 0
+stack = []
+checkzero = False
+
+for _ in range(N):
+    x,y = map(int,input().split())
+    if y == 0 : checkzero = True
+    while True:
+        if not stack:
+            stack.append(y)
+            break
+        else:
+            if stack[-1] > y:
+                stack.pop()
+                answer += 1
+                continue
+            elif stack[-1] == y:
+                break
+            else:
+                stack.append(y)
+                break
+
+
+answer += len(stack)
+if checkzero : answer -= 1
+print(answer)
+
